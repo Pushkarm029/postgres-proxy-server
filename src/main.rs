75e597613d3db7961d9a8b80e99701e7c9970bae
Pushkar Mishra <pushkarmishra029@gmail.com>
2024-09-23 19:07:05 +0530

    test not fixed
    Signed-off-by: Pushkar Mishra <pushkarmishra029@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,7 @@ mod utils;
 use crate::processor::ProcessorFactory;
 use crate::utils::config::get_server_binding_address;
 
-#[tokio::main]
-async fn main() {
+async fn run_tcp_server() {
     env_logger::init();
     let factory = Arc::new(ProcessorFactory::new().await);
     let server_address = get_server_binding_address();
@@ -45,61 +44,7 @@ async fn main() {
     }
 }
 
-// use log::{error, info};
-// use std::sync::Arc;
-// use tokio::net::TcpListener;
-// use tokio::sync::oneshot;
-
-// mod processor;
-// mod query_handler;
-// mod schema;
-// #[cfg(test)]
-// mod tests;
-// mod utils;
-
-// use crate::processor::ProcessorFactory;
-// use crate::utils::config::get_server_binding_address;
-
-// async fn run_server(mut shutdown: oneshot::Receiver<()>) -> Result<(), Box<dyn std::error::Error>> {
-//     let factory = Arc::new(ProcessorFactory::new().await);
-//     let server_address = get_server_binding_address();
-
-//     info!("Starting server at {}", server_address);
-
-//     let listener = TcpListener::bind(server_address.clone()).await?;
-
-//     info!("Listening for connections on {}", server_address);
-
-//     loop {
-//         tokio::select! {
-//             accept_result = listener.accept() => {
-//                 match accept_result {
-//                     Ok((tcp_stream, addr)) => {
-//                         info!("New connection accepted from: {}", addr);
-//                         let factory_ref = factory.clone();
-//                         tokio::spawn(async move {
-//                             pgwire::tokio::process_socket(tcp_stream, None, factory_ref).await
-//                         });
-//                     }
-//                     Err(e) => {
-//                         error!("Failed to accept connection: {}", e);
-//                     }
-//                 }
-//             }
-//             _ = &mut shutdown => {
-//                 info!("Shutdown signal received. Stopping server.");
-//                 break;
-//             }
-//         }
-//     }
-
-//     Ok(())
-// }
-
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     env_logger::init();
-
-//     let (_, shutdown_receiver) = oneshot::channel::<()>();
-//     run_server(shutdown_receiver).await
-// }
+#[tokio::main]
+async fn main() {
+    run_tcp_server().await;
+}
--- src/tests.rs
+++ src/tests.rs
@@ -2,14 +2,17 @@ use std::thread;
 
 #[cfg(test)]
 use super::*;
-use sqlx::{postgres::PgPoolOptions, query, Postgres};
+use sqlx::{postgres::PgPoolOptions, query};
+use tokio::runtime::Builder;
 use tokio_postgres::NoTls;
 
 const DB_ADDRESS: &str = "postgres://postgres:postgres@localhost:5432";
 const SCHEMA_DB_ADDRESS: &str = "postgres://postgres:postgres@localhost:5432/test_schema";
 const TEST_DB_ADDRESS: &str = "postgres://postgres:postgres@localhost:5432/test_db";
 
-const PROXY_DB_ADDRESS: &str = "postgres://postgres:postgres@127.0.0.1:5433/test_db";
+// const PROXY_DB_ADDRESS: &str = "postgres://postgres:postgres@127.0.0.1:5433/test_db";
+
+const PROXY_DB_ADDRESS: &str = "postgres://postgres:postgres@127.0.0.1:5433/main";
 
 async fn init_db(pool: &sqlx::PgPool, name: &str) -> sqlx::Result<()> {
     sqlx::query(format!("CREATE DATABASE {name}").as_str())
@@ -93,117 +96,223 @@ async fn cleanup() {
         .unwrap();
 }
 
-#[sqlx::test]
-async fn e2e_test() {
-    // Setup schema database
-    // let schema_creation_pool = PgPoolOptions::new().connect(DB_ADDRESS).await.unwrap();
-    // init_db(&schema_creation_pool, "test_schema").await.unwrap();
+// #[sqlx::test]
 
-    // let schema_population_pool = PgPoolOptions::new()
-    //     .connect(SCHEMA_DB_ADDRESS)
-    //     .await
-    //     .unwrap();
-    // populate_schema_db(&schema_population_pool).await.unwrap();
+// #[tokio::test]
+// async fn e2e_test() {
+//     // Setup schema database
+//     // let schema_creation_pool = PgPoolOptions::new().connect(DB_ADDRESS).await.unwrap();
+//     // init_db(&schema_creation_pool, "test_schema").await.unwrap();
 
-    // let main_creation_pool = PgPoolOptions::new().connect(DB_ADDRESS).await.unwrap();
-    // init_db(&main_creation_pool, "test_db").await.unwrap();
+//     // let schema_population_pool = PgPoolOptions::new()
+//     //     .connect(SCHEMA_DB_ADDRESS)
+//     //     .await
+//     //     .unwrap();
+//     // populate_schema_db(&schema_population_pool).await.unwrap();
 
-    // let main_population_pool = PgPoolOptions::new().connect(TEST_DB_ADDRESS).await.unwrap();
+//     // let main_creation_pool = PgPoolOptions::new().connect(DB_ADDRESS).await.unwrap();
+//     // init_db(&main_creation_pool, "test_db").await.unwrap();
 
-    // setup_main_db(&main_population_pool).await.unwrap();
+//     // let main_population_pool = PgPoolOptions::new().connect(TEST_DB_ADDRESS).await.unwrap();
 
-    // Start the server in a separate task
-    // let server = tokio::spawn(async {
-    //     main();
-    // });
-    // main();
+//     // setup_main_db(&main_population_pool).await.unwrap();
 
-    // Run TCP Server in another runtime
-    let handler = thread::spawn(|| main());
-    // let _some = thread::spawn(|| {
-    //     main();
-    // }).join().expect("Thread Panicked");
+//     // Start the server in a separate task
+//     // let server: tokio::task::JoinHandle<()> = tokio::spawn(async {
+//     //     main(); // Ensure `main` is async or call the appropriate async function.
+//     // });
 
-    // let (shutdown_sender, shutdown_receiver) = oneshot::channel();
-    // let server_task = tokio::spawn(async move {
-    //     run_server(shutdown_receiver).await.expect("Server error");
-    // });
+//     // main();
 
-    // Give the server some time to start
-    tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+//     // Run TCP Server in another runtime
 
-    let proxy_pool = PgPoolOptions::new()
-        .connect(PROXY_DB_ADDRESS)
-        .await
-        .unwrap();
+//     // let handler = thread::spawn(|| {
+//     //     let runtime = Builder::new_multi_thread()
+//     //         .worker_threads(4)
+//     //         .thread_name("my-custom-name")
+//     //         .thread_stack_size(3 * 1024 * 1024)
+//     //         .build()
+//     //         .unwrap();
 
-    // handler.join();
-    // let pool: tokio::task::JoinHandle<Pool<Postgres>> = tokio::spawn(async {
-    //     PgPoolOptions::new().connect(PROXY_DB_ADDRESS).await.unwrap()
-    // });
+//     //     runtime.block_on(async {
+//     //         main();
+//     //     });
+//     // });
+//     // main();: tokio::task::JoinHandle<()> = tokio::spawn(async {
+//     //     main(); // Ensure `main` is async or call the appropriate async function.
+//     // });
+//     // let _some = thread::spawn(|| {
+//     //     main();
+//     // }).join().expect("Thread Panicked");
 
-    // Execute queries with MEASURE function
-    // let head_count: (i64,) = sqlx::query_as("SELECT COUNT(id) FROM employees GROUP BY name;")
-    //     .fetch_one(&proxy_pool)
-    //     .await
-    //     .unwrap();
+//     // let (shutdown_sender, shutdown_receiver) = oneshot::channel();
+//     // let server_task = tokio::spawn(async move {
+//     //     run_server(shutdown_receiver).await.expect("Server error");
+//     // });
 
-    // let revenue: (f64,) = sqlx::query_as("SELECT MEASURE(revenue) AS total_revenue")
-    //     .fetch_one(&proxy_pool)
-    //     .await
-    //     .unwrap();
+//     // Give the server some time to start
+//     tokio::spawn(async {
+//         main();
+//     });
 
-    // let avg_salary: (f64,) = sqlx::query_as("SELECT MEASURE(average_salary) AS avg_salary")
-    //     .fetch_one(&proxy_pool)
-    //     .await
-    //     .unwrap();
+//     let (client, connection) = tokio_postgres::connect(PROXY_DB_ADDRESS, NoTls)
+//         .await
+//         .unwrap();
 
-    // Check the results
-    // assert_eq!(head_count.0, 3);
-    // assert_eq!(revenue.0, 3700.0);
-    // assert_eq!(avg_salary.0, 55000.0);
+//     tokio::spawn(async move {
+//         if let Err(e) = connection.await {
+//             eprintln!("connection error: {}", e);
+//         }
+//     });
 
-    // Test a regular SELECT query
+//     tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
 
-    let sss = query("SELECT * FROM users;")
-        .fetch_all(&proxy_pool)
-        .await
-        .unwrap();
-    // let (client, _connection) = tokio_postgres::connect(PROXY_DB_ADDRESS, NoTls)
-    // .await
-    // .expect("Failed to connect to database");
+//     let sss = client.query("SELECT * FROM employees;", &[]).await.unwrap();
+//     info!("{:?}", sss);
+//     // let stmt = client
+//     //     .prepare(query)
+//     //     .await
+//     //     .map_err(|e| PgWireError::ApiError(Box::new(e)))?;
+//     // let rows = client
+//     //     .query(&stmt, &[])
+//     //     .await
+//     //     .map_err(|e| PgWireError::ApiError(Box::new(e)))?;
 
-    // // client.execute(statement, params)
-    // let stmt = client.prepare("SELECT * FROM users ORDER BY id;").await.unwrap();
-    // let sss = client.query(&stmt, &[]).await.unwrap();
+//     // let proxy_pool = PgPoolOptions::new()
+//     //     .connect(PROXY_DB_ADDRESS)
+//     //     .await
+//     //     .unwrap();
 
-    // println!("{:?}",sss);
-    info!("{:?}", sss.len());
-    info!("{:?}", sss);
+//     // handler.join();
+//     // let pool: tokio::task::JoinHandle<Pool<Postgres>> = tokio::spawn(async {
+//     //     PgPoolOptions::new().connect(PROXY_DB_ADDRESS).await.unwrap()
+//     // });
 
-    // let users: Vec<(i32, String, String, f64, f64)> =
-    //     sqlx::query_as("SELECT * FROM users ORDER BY id;")
-    //         .fetch_all(&proxy_pool)
-    //         .await
-    //         .unwrap();
+//     // Execute queries with MEASURE function
+//     // let head_count: (i64,) = sqlx::query_as("SELECT COUNT(id) FROM employees GROUP BY name;")
+//     //     .fetch_one(&proxy_pool)
+//     //     .await
+//     //     .unwrap();
 
-    // assert_eq!(users.len(), 3);
-    // assert_eq!(users[0].1, "John Doe");
-    // assert_eq!(users[1].1, "Jane Smith");
-    // assert_eq!(users[2].1, "Alice Johnson");
+//     // let revenue: (f64,) = sqlx::query_as("SELECT MEASURE(revenue) AS total_revenue")
+//     //     .fetch_one(&proxy_pool)
+//     //     .await
+//     //     .unwrap();
 
-    // // Clean up
-    // shutdown_sender
-    //     .send(())
-    //     .expect("Failed to send shutdown signal");
+//     // let avg_salary: (f64,) = sqlx::query_as("SELECT MEASURE(average_salary) AS avg_salary")
+//     //     .fetch_one(&proxy_pool)
+//     //     .await
+//     //     .unwrap();
 
-    // // Wait for the server to shut down
-    // server_task.await.expect("Server task panicked");
-    // cleanup().await;
-    // server.abort();
-}
+//     // Check the results
+//     // assert_eq!(head_count.0, 3);
+//     // assert_eq!(revenue.0, 3700.0);
+//     // assert_eq!(avg_salary.0, 55000.0);
+
+//     // Test a regular SELECT query
+
+//     // let sss = query("SELECT * FROM users;")
+//     //     .fetch_all(&proxy_pool)
+//     //     .await
+//     //     .unwrap();
+
+//     // let sss = sqlx::query("CREATE database fff;").execute(&proxy_pool).await.unwrap();
+//     // let (client, _connection) = tokio_postgres::connect(PROXY_DB_ADDRESS, NoTls)
+//     // .await
+//     // .expect("Failed to connect to database");
+
+//     // // client.execute(statement, params)
+//     // let stmt = client.prepare("SELECT * FROM users ORDER BY id;").await.unwrap();
+//     // let sss = client.query(&stmt, &[]).await.unwrap();
+
+//     // println!("{:?}",sss);
+//     // info!("{:?}", sss);
+//     // handler.join().unwrap();
+//     // let users: Vec<(i32, String, String, f64, f64)> =
+//     //     sqlx::query_as("SELECT * FROM users ORDER BY id;")
+//     //         .fetch_all(&proxy_pool)
+//     //         .await
+//     //         .unwrap();
+
+//     // assert_eq!(users.len(), 3);
+//     // assert_eq!(users[0].1, "John Doe");
+//     // assert_eq!(users[1].1, "Jane Smith");
+//     // assert_eq!(users[2].1, "Alice Johnson");
+
+//     // // Clean up
+//     // shutdown_sender
+//     //     .send(())
+//     //     .expect("Failed to send shutdown signal");
+
+//     // // Wait for the server to shut down
+//     // server_task.await.expect("Server task panicked");
+//     // cleanup().await;
+//     // server.abort();
+// }
 
 // not implemented: Extended Query is not implemented on this server.
 // note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
 // thread 'tests::e2e_test' panicked at src/tests.rs:170:14:
 // called `Result::unwrap()` on an `Err` value: Io(Custom { kind: UnexpectedEof, error: "expected to read 5 bytes, got 0 bytes at EOF" })
+
+// #[sqlx::test]
+// async fn e2e_test() {
+//     thread::spawn(|| main());;
+//     tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+
+//     let proxy_pool = PgPoolOptions::new()
+//         .connect(PROXY_DB_ADDRESS)
+//         .await
+//         .unwrap();
+
+//     let sss = query("SELECT * FROM users;")
+//         .fetch_all(&proxy_pool)
+//         .await
+//         .unwrap();
+//     info!("{:?}", sss);
+// }
+
+async fn start_server() {
+    // Spawn the main function
+    let _main_handle = tokio::spawn(async {
+        run_tcp_server().await;
+    });
+
+    // Wait a bit for the server to start
+    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+}
+
+#[tokio::test]
+async fn e2e_test() {
+    let server_handle = tokio::spawn(async {
+        run_tcp_server().await;
+    });
+
+    // Add a delay to ensure the server is fully up and running before connecting
+    tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+
+    // Connect to the PostgreSQL server
+    let (client, connection) = tokio_postgres::connect(PROXY_DB_ADDRESS, NoTls)
+        .await
+        .expect("Failed to connect to the database");
+
+    let conn_handler = tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("Connection error: {}", e);
+        }
+    });
+
+    // let res = client.query("SELECT * FROM employees;", &[]).await.unwrap();
+    // println!("{res:?}");
+
+    let query_handler = tokio::spawn(async move{
+        let res = client.query("SELECT * FROM employees;", &[]).await.unwrap();
+        info!("{res:?}");
+    });
+
+    tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+
+    query_handler.await.unwrap();
+    conn_handler.abort();
+    server_handle.abort();
+}
